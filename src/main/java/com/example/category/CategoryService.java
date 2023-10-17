45d47fed1ef3849a29b4a922ddb57d3534f46784
knmyusk <Konomi.Yosuke@technopro.com>
2023-10-17 16:07:24 +0900

    カテゴリーのレポジトリインターフェースを作成、サービスクラスの修正

--- src/main/java/com/example/category/CategoryService.java
+++ src/main/java/com/example/category/CategoryService.java
@@ -2,6 +2,7 @@ package com.example.category;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.entity.Category;
@@ -9,15 +10,27 @@ import com.example.entity.Category;
 @Service
 public class CategoryService {
 	
+	private final CategoryRepository categoryRepository;
+	
+	@Autowired
+	public CategoryService(CategoryRepository categoryRepository) {
+		this.categoryRepository = categoryRepository;
+	}
+	
 	/**
 	 * カテゴリー情報全件取得
 	 * 
 	 * @return カテゴリー情報のリスト
 	 */
 	
-	public List<Category> listAll(){
+	public List<Category> listAll(String keyword){
 		// 検索キーワードがあった場合
+		if(keyword != null && !keyword.isEmpty()) {
+			return categoryRepository.search(keyword);
+		}
 		// それ以外の場合
-		return null;
+		else {
+			return categoryRepository.findAll();
+		}
 	}
 }
