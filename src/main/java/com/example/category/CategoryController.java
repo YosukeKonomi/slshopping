b42ce242af35a0f81735c8a801a9208c54c6197f
knmyusk <Konomi.Yosuke@technopro.com>
2023-10-17 16:49:56 +0900

    カテゴリーのコントローラーとモックの修正

--- src/main/java/com/example/category/CategoryController.java
+++ src/main/java/com/example/category/CategoryController.java
@@ -1,14 +1,26 @@
 package com.example.category;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.entity.Category;
+
 @Controller
 @RequestMapping("/categories")
 public class CategoryController {
+	
+	private final CategoryService categoryService;
+	
+	@Autowired
+	public CategoryController(CategoryService categoryService) {
+		this.categoryService = categoryService;
+	}
 	
 	/**
 	 * カテゴリー一覧画面表示
@@ -19,7 +31,9 @@ public class CategoryController {
 	@GetMapping
 	public String listCategories(@RequestParam(required = false) String keyword, Model model) {
 		// 全カテゴリー情報の取得
-		
+		List<Category> listCategories = categoryService.listAll(keyword);
+		model.addAttribute("listCategories", listCategories);
+		model.addAttribute("keyword", keyword);
 		// カテゴリー一覧画面を渡す
 		return "categories/categories";
 	}
